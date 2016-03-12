dc3b9a6dc635ea1616beac71f86b5bbc37388b41
Alastair Paragas <alastairparagas@gmail.com>
2016-03-12 15:32:55 -0500

    JS Hint Task

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,40 @@
 var gulp = require('gulp'),
     gulpWatch = require('gulp-watch'),
     gulpSass = require('gulp-sass'),
-    
+    gulpJshint = require('gulp-jshint'),
+
     chalk = require('chalk');
 
 
-// Compile SASS to CSS
-gulp.task('sass', function () {
-    
-    function sassTask() {
-        gulp.src('./ui/scss/**/main.scss')
-            .pipe(gulpSass())
-            .on('error', function () {
-            console.log(chalk.bgRed.red.bold("SASS Compilation Error"));
-        });
-    }
-    
+function sassTask() {
+    gulp.src('./ui/scss/**/main.scss')
+        .pipe(gulpSass())
+        .on('error', function () {
+        console.log(chalk.bgRed.white.bold("SASS Compilation Error"));
+    })
+        .pipe(gulp.dest('./ui/dist/'));
+
+    console.log(chalk.bgGreen.white.bold("SASS Compilation Done ---"));
+}
+gulp.task('sass', sassTask);
+gulp.task('sass-watch', function () {
     sassTask();
     gulpWatch('./ui/scss/**/*.scss', sassTask);
-    
 });
+
+
+function jshintTask() {
+    gulp.src(['./ui/**/*.js', 'main.js', 'gulpfile.js'])
+        .pipe(gulpJshint())
+        .pipe(gulpJshint.reporter('jshint-stylish'))
+        .pipe(gulp.dest('./ui/dist/'));
+
+    console.log(chalk.bgGreen.white.bold('JS Hint Done ---'));
+}
+gulp.task('jshint', jshintTask);
+gulp.task('jshint-watch', function () {
+    jshintTask();
+    gulpWatch(['./ui/**/*.js', 'main.js', 'gulpfile.js']);
+});
+
+gulp.task('build', ['sass']);
